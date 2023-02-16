14241c4bc2ed00cea5ff492f573b1cdf0563924d
mota <MotaGodkiller>
2023-02-16 04:48:50 +0330

    add nostr broadcast

---
+++ js/relays.js
@@ -0,0 +1,22 @@
+// list of paid relays from:
+// https://thebitcoinmanual.com/articles/paid-nostr-relay/
+
+const relays = [
+	'wss://nostr.orangepill.dev', 
+	'wss://jiggytom.ddns.net',
+	'wss://eden.nostr.land', 
+	'wss://nostr.fmt.wiz.biz',
+	'wss://relay.damus.io', 
+	'wss://nostr-pub.wellorder.ner', 
+	'wss://relay.nostr.info',
+	'wss://nostr.bitcoiner.social',
+	'wss://nostr.onsats.org',
+	'wss://nos.lol',
+	'wss://brb.io',
+	'wss://relay.snort.social',
+	'wss://relay.current.fyi',
+	'wss://nostr.relayer.se',
+	'wss://nostr-relay.freeberty.net',
+	'wss://nostr-relay.wlvs.space'
+]
+
